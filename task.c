28eb235f76d9459244b87f120f9dfb87acaef060
Mai Van Manh <maivanmanh@tdtu.edu.vn>
2025-10-26 20:44:36 +0700

    fix

--- task.c
+++ task.c
@@ -2,7 +2,7 @@
 
 void reverseArray(int arr[], int n) {
     int temp;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n / 2; i++) {
         temp = arr[i];
         arr[i] = arr[n - i - 1];
         arr[n - i - 1] = temp;
